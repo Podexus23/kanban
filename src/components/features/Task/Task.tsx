206e744ca7e1dc8ff91186892ab815e1cf277a50
Podexus23 <santon23@gmail.com>
2025-05-06 15:42:20 +0300

    feat: task doing brr brr

--- src/components/features/Task/Task.tsx
+++ src/components/features/Task/Task.tsx
@@ -1,7 +1,39 @@
+import { useRef, useState } from "react";
 import styles from "./Task.module.css";
 
-const Task = ({ task }) => {
-  return <div className={styles.task}>{task}</div>;
+const Task = ({ task, dragOver, onDraggable, onDragEnd }) => {
+  const handleDragStart = (e, id) => {
+    onDraggable(id);
+    e.dataTransfer.effectAllowed = "move";
+    console.log("start");
+    console.log(e.target);
+  };
+
+  const handleDragEnd = (e) => {
+    onDraggable(null);
+    dragOver = null;
+    console.log("end");
+    console.log(e.target);
+  };
+
+  const handleDragOver = (e, id) => {
+    e.preventDefault();
+    dragOver.current = id;
+    // console.log("over");
+    // console.log(dragOver);
+  };
+
+  return (
+    <div
+      className={styles.task}
+      draggable
+      onDragStart={(e) => handleDragStart(e, task.id)}
+      onDragOver={(e) => handleDragOver(e, task.id)}
+      onDragEnd={(e) => onDragEnd(e, task.id)}
+    >
+      {task.text}
+    </div>
+  );
 };
 
 export default Task;
--- src/components/features/desk/Desk.tsx
+++ src/components/features/desk/Desk.tsx
@@ -1,11 +1,18 @@
 // - Стандартные колонки: "To Do", "In Progress", "Done" ✔️
 // - Возможность добавлять/удалять/переименовывать колонки ✔️
-// - Горизонтальный скролл или адаптивное отображение колонок
-import { useState } from "react";
+// - Горизонтальный скролл или адаптивное отображение колонок ✔️
+import { useEffect, useRef, useState } from "react";
 import styles from "./Desk.module.css";
 import { faker } from "@faker-js/faker";
 import Task from "../Task/Task";
 import Button from "../../Button";
+
+const makeTask = () => {
+  return {
+    text: faker.lorem.lines({ min: 1, max: 1 }),
+    id: faker.string.uuid(),
+  };
+};
 
 function DeskHeader({ title, onDeleteDesk, onRenameDeskTitle }) {
   const [isRename, setIsRename] = useState(false);
@@ -64,24 +71,54 @@ function DeskHeader({ title, onDeleteDesk, onRenameDeskTitle }) {
 }
 
 function Desk({ deskTitle, onDeleteDesk, onRenameDeskTitle }) {
-  //!remove
-  const taskToTest = Array.from({ length: 4 }, (_, index) => "");
+  const [tasks, setTasks] = useState(
+    Array.from({ length: 4 }, (_, index) => makeTask())
+  );
+  const [draggable, setDraggable] = useState(null);
+  const dragOver = useRef(null);
+
+  const handleAddTask = () => {
+    setTasks((tasks) => [...tasks, makeTask()]);
+  };
+
+  const handleTaskDragEnd = (e) => {
+    const toMoveIndex = tasks.findIndex((task) => task.id == dragOver.current);
+    const draggableIndex = tasks.findIndex((task) => task.id == draggable);
+
+    const newTasks = [...tasks];
+    newTasks.splice(draggableIndex, 1);
+    // Вставляем элемент на новую позицию
+    newTasks.splice(toMoveIndex, 0, tasks[draggableIndex]);
+    setTasks(newTasks);
+
+    setDraggable(null);
+    dragOver.current = null;
+    console.log("end");
+  };
 
   return (
-    <div className={styles.desk}>
+    <div
+      className={styles.desk}
+      // onDragOver={() => console.log(dragOver.current)}
+    >
       <DeskHeader
         title={deskTitle}
         onDeleteDesk={onDeleteDesk}
         onRenameDeskTitle={onRenameDeskTitle}
       />
-      <Button name={"+ add new task"} size={"medium"} />
+      <Button onClick={handleAddTask} name={"+ add new task"} size={"medium"} />
       <main>
         <div className={styles.tasklist}>
-          {taskToTest.map((task) => (
-            <Task task={faker.lorem.lines({ min: 1, max: 2 })} />
+          {tasks.map((task) => (
+            <Task
+              task={task}
+              key={task.id}
+              dragOver={dragOver}
+              onDraggable={setDraggable}
+              onDragEnd={handleTaskDragEnd}
+            />
           ))}
         </div>
-        {/* <div>{faker.lorem.lines({ min: 1, max: 2 })}</div> */}
       </main>
     </div>
   );
