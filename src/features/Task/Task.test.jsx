d3aa595d2bfc8c797c4ee2f503c4ac33e2edbaa3
Podexus23 <santon23@gmail.com>
2025-05-27 01:32:52 +0300

    test: add test for title change

--- src/features/Task/Task.test.jsx
+++ src/features/Task/Task.test.jsx
@@ -1,6 +1,7 @@
 import { describe, expect, test, vi } from "vitest";
 import Task from "./Task";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("test tasks functionality", () => {
   test("render task properly", async () => {
@@ -16,5 +17,31 @@ describe("test tasks functionality", () => {
 
     const buttonClose = await screen.findByRole("button", { name: "❌" });
     expect(buttonClose).toBeInTheDocument();
+  });
+
+  test("task change title", async () => {
+    render(
+      <Task
+        task={{ title: "title", text: "text", id: 23 }}
+        onEditTask={vi.fn()}
+      />
+    );
+
+    //check button
+    const editButtons = screen.getAllByRole("button", { name: "✏️" });
+    expect(editButtons[0]).toBeInTheDocument();
+
+    //click test
+    await userEvent.click(editButtons[0]);
+    const titleInput = await screen.findByRole("textbox");
+    expect(titleInput).toBeInTheDocument();
+
+    // await userEvent.type(titleInput, " updated");
+    fireEvent.change(titleInput, { target: { value: "title updated" } });
+
+    const doneEditButton = await screen.findByRole("button", { name: "✔️" });
+    await userEvent.click(doneEditButton);
+    const title = await screen.findByText("title updated", { exact: true });
+    expect(title).toBeInTheDocument();
   });
 });
